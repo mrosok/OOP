d47a2990f9d3a53612efa71d637c33e4567907cf
Unknown <martin.rosok@gmail.com>
2018-01-26 21:54:41 +0100

    Ferdigstilt oblig 2

--- Oblig2/src/no/hiof/martr/Main.java
+++ Oblig2/src/no/hiof/martr/Main.java
@@ -1,8 +1,6 @@
 package no.hiof.martr;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
 
@@ -21,44 +19,40 @@ public class Main {
         System.out.println(episode2.toString());
 
         //oppgave 4 - lag 5 sesonger med 20 episoder og list episoder i sesong 4
-        createEpisodes(GameOfThrones, 5,20);
+        GameOfThrones.createEpisodes(5,20);
         GameOfThrones.listEpisodes(4);
 
         //oppgave 6 - print ut gjennomsnittlig spilletid
         System.out.println("Average runtime: " + GameOfThrones.getAvgRuntime() + " minutes");
 
-        // alternativt setRuntime i stedet for å oppdatere oppgave 4
-        // ArrayList<Episode> episodes = GameOfThrones.getEpisodes();
-        // for(int i=0; i<episodes.size(); i++) {
-        //     episodes.get(i).setRuntime(randomRuntime());
-        // }
-
         //oppgave 8 - test implementasjon av oppgave 7 (validering)
         System.out.println("Number of seasons in " + GameOfThrones.getTitle() + ": " + GameOfThrones.getNumberOfSeasons());
-        //legger til episode i gyldig sesong 6 - får korrekt output fra System.out
-        Episode episode3 = new Episode(1,6,"Larseknu in trouble",LocalDate.of(2018,1,21));
-        GameOfThrones.addEpisode(episode3);
-        System.out.println("Number of seasons in " + GameOfThrones.getTitle() + ": " + GameOfThrones.getNumberOfSeasons());
 
-        //legger til episode med ugyldig sesong 8 - får feilmelding fra System.out
-        Episode episode4 = new Episode(1,8,"Larseknu saves the day",LocalDate.of(2018,1,22));
-        GameOfThrones.addEpisode(episode4);
-        System.out.println("Number of seasons in " + GameOfThrones.getTitle() + ": " + GameOfThrones.getNumberOfSeasons());
+            //legger til episode i gyldig sesong 6 - får korrekt output fra System.out
+            Episode episode3 = new Episode(1,6,"Larseknu in trouble",LocalDate.of(2018,1,21));
+            GameOfThrones.addEpisode(episode3);
+            System.out.println("Number of seasons in " + GameOfThrones.getTitle() + ": " + GameOfThrones.getNumberOfSeasons());
 
-    }
-    //metode til oppgave 4 (oppdatert i oppgave 6 for å legge random runtime)
-    public static void createEpisodes(TVSeries tvseries, int seasons, int episodesPerSeason) {
-        for (int i = 1; i <= seasons; i++) {
-            for (int j = 1; j <= episodesPerSeason; j++) {
-                Episode episode = new Episode(j, i, "Episode " + j, LocalDate.of(2000, 1, j + 1),randomRuntime());
-                tvseries.addEpisode(episode);
-            }
-        }
-    }
-    //metode til oppgave 6, gir tilfeldig int mellom 20 og 30
-    public static int randomRuntime() {
-        Random rand = new Random();
-        int n = rand.nextInt(30) + 20;
-        return n;
+            //legger til episode med ugyldig sesong 8 - får feilmelding fra System.out
+            Episode episode4 = new Episode(1,8,"Larseknu saves the day",LocalDate.of(2018,1,22));
+            GameOfThrones.addEpisode(episode4);
+            System.out.println("Number of seasons in " + GameOfThrones.getTitle() + ": " + GameOfThrones.getNumberOfSeasons());
+
     }
 }
+
+/* OPPGAVE 9
+
+    CLASS = en "blueprint" for et objekt. Inneholder variabler og metoder som beskriver tilstanden og egenskapene til et objekt.
+    OBJECT = en instanse av en klasse (Class), som får alle egenskapene beskrevet i klassen.
+    MEMBER/FIELD VARIABLE = variabler i en klasse som beskriver tilstanden til et objekt.
+    OVERLOADING = Flere konstruktører og metoder kan ha samme navn hvis parameter-listene deres er forskjellige.
+    OVERRIDING = En arvet metode som er overskrevet/modifisert.
+    EXTENDS = Å arve fra en annen klasse. Gjeldene klasse får da tilgang på funksjonaliteten fra klassen det "extendes" fra.
+    POLYMORPHISM =
+    PRIVATE/PUBLIC etc = Private vil si at metoden/feltvariabelen bare er tilgjengelig i sin egen klasse. Public derimot er tilgjengelig for alle utenfor egen klasse.
+                         Protected kan nås av alle i sin egen pakke, og i tillegg av sub-klasser av sin klasse i andre pakker.
+    THIS/SUPER = this referer til "denne" instansen av en klasse - objektet hvor i metoden/konstruktøren blir kalt.
+                 super referer til "superklassen" eller "foreldreklassen" til en klasse, altså klassen det er direkte arvet fra. Vi kan da referere til en feltvariabel,                metode eller konstruktør
+
+*/
--- Oblig2/src/no/hiof/martr/TVSeries.java
+++ Oblig2/src/no/hiof/martr/TVSeries.java
@@ -1,14 +1,14 @@
 package no.hiof.martr;
 
-import no.hiof.martr.Episode;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Random;
 
 //oppgave 1 - lag klasse
 public class TVSeries {
     private String title;
     private String description;
-    //synes det er mer fornuftig å ha bare år som utgivelsesinfo for TV-serie, og heller dato på enkeltepisoder
+    // synes det er mer fornuftig å ha bare år som utgivelsesinfo for TV-serie, og heller dato på enkeltepisoder
     private int releaseYear;
     private ArrayList<Episode> episodes = new ArrayList<>();
     private int avgRuntime;
@@ -20,6 +20,70 @@ public class TVSeries {
         this.releaseYear = releaseYear;
         this.numberOfSeasons = 1;
     }
+
+    public void addEpisode(Episode episode) {
+
+        //oppdatert validering for oppgave 7
+        if (episode.getSeason() > this.numberOfSeasons + 1) {
+            System.out.println("Season number exceeds limit");
+        } else if (episode.getSeason() == this.numberOfSeasons + 1) {
+            this.numberOfSeasons++;
+            this.episodes.add(episode);
+        } else {
+            this.episodes.add(episode);
+        }
+        //oppgave 5 - inkludert updateAvgRuntime
+        updateAvgRuntime();
+    }
+
+    //oppgave 3 - override toString()
+    @Override
+    public String toString() {
+        return this.title + "(" + this.releaseYear + ") / " + this.numberOfSeasons + (this.numberOfSeasons > 1 ? " seasons" : " season");
+    }
+
+    //oppgave 4 - liste av alle episoder
+    public void listEpisodes(int season) {
+        for (int i = 0; i < episodes.size(); i++) {
+            if (episodes.get(i).getSeason() == season) {
+                System.out.println(episodes.get(i).toString());
+            }
+        }
+    }
+    //metode til oppgave 4 (oppdatert i oppgave 6 for å legge random runtime)
+    public void createEpisodes(int seasons, int episodesPerSeason) {
+        for (int i = 1; i <= seasons; i++) {
+            for (int j = 1; j <= episodesPerSeason; j++) {
+                Episode episode = new Episode(j, i, "Episode " + j, LocalDate.of(2000, 1, j + 1), randomRuntime());
+                this.addEpisode(episode);
+            }
+        }
+    }
+
+    //oppgave 5 - kalkuler gjennomsnittlig spilletid
+    public void updateAvgRuntime() {
+        int sum = 0;
+        for (int i = 0; i < episodes.size(); i++) {
+            sum += episodes.get(i).getRuntime();
+        }
+        this.avgRuntime = sum / episodes.size();
+    }
+
+    //oppgave 6 - gir tilfeldig int mellom 20 og 30
+    public static int randomRuntime() {
+        Random rand = new Random();
+        int n = rand.nextInt(30) + 20;
+        return n;
+    }
+
+/*  (oppgave 6) alternativ setRuntime i stedet for å oppdatere oppgave 4
+
+    ArrayList<Episode> episodes = GameOfThrones.getEpisodes();
+    for(int i=0; i<episodes.size(); i++) {
+        episodes.get(i).setRuntime(randomRuntime());
+    }
+*/
+
 
     public String getTitle() {
         return this.title;
@@ -57,46 +121,8 @@ public class TVSeries {
         return avgRuntime;
     }
 
-    public void addEpisode(Episode episode) {
-
-        //oppdatert validering for oppgave 7
-        if(episode.getSeason() > this.numberOfSeasons + 1) {
-            System.out.println("Season number exceeds limit");
-        } else if (episode.getSeason() == this.numberOfSeasons + 1){
-            this.numberOfSeasons++;
-            this.episodes.add(episode);
-        } else {
-            this.episodes.add(episode);
-        }
-        //oppgave 5 - inkludert updateAvgRuntime
-        updateAvgRuntime();
-    }
-
-    //oppgave 3 - override toString()
-    //la også til antall sesonger fra oppgave 7
-    @Override
-    public String toString() {
-        return this.title + "(" + this.releaseYear + ") / " + this.numberOfSeasons + (this.numberOfSeasons>1? " seasons":" season") ;
-    }
-
-    //oppgave 4 - liste av alle episoder
-    public void listEpisodes(int season) {
-        for (int i = 0; i < episodes.size(); i++) {
-            if (episodes.get(i).getSeason() == season) {
-                System.out.println(episodes.get(i).toString());
-            }
-        }
-    }
-
-    //oppgave 5 - kalkuler gjennomsnittlig spilletid
-    public void updateAvgRuntime() {
-        int sum = 0;
-        for (int i = 0; i < episodes.size(); i++) {
-            sum += episodes.get(i).getRuntime();
-        }
-        this.avgRuntime = sum / episodes.size();
-    }
 }
+
 
 
 
